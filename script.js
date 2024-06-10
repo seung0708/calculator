9bd3d918e04c1d0f8d6286e80e021ac8ec009cb3
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-06-10 09:57:50 -0700

    currently trying to figure out why decimal keeps displaying when it shouldnt

--- script.js
+++ script.js
@@ -1,45 +1,16 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
-
+const display = document.querySelector('.calculator__display');
 
 //1. listen for all keypresses
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         const key = e.target;
-        const action = key.dataset.action; 
-        //if the key does not have a data-action, it must be a number key
-        if(!action) {
-            console.log('number key')
-        }
-        //if  the key has a data-action that is either add, subtract, multiply, or divide, we know the key is an operator
-        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-            console.log('operator key!')
-        }
-        //if key's data-action is decimal, we know the use ther clicked decmial
-        if (action === 'decimal') {
-            console.log('decimal key!')
-          }
-          
-          if (action === 'clear') {
-            console.log('clear key!')
-          }
-          
-          if (action === 'calculate') {
-            console.log('equal key!')
-          }
-    }
-})
-
-const display = document.querySelector('.calculator__display');
-
-keys.addEventListener('click', e => {
-    if(e.target.matches('button')) {
-        const key = e.target;
         const action = key.dataset.actoin;
         const keyContent = key.textContent;
         const displayedNum = display.textContent;
         const previousKeyType = calculator.dataset.previousKeyType
-        //if the calculator shows 0, we want to replace the calculator's display with the clicked key. 
+        //if the key does not have a data-action, it must be a number key
         if (!action) {
             if(displayedNum === '0' || previousKeyType === 'operator') {
                 display.textContent = keyContent
@@ -47,12 +18,20 @@ keys.addEventListener('click', e => {
                 //if the calculator shows a non-zero number, we want to append the clicked key to the displayed number
                 display.textContent = displayedNum + keyContent;
             }
+            calculator.dataset.previousKeyType = 'number'
         }
 
         if (action === 'decimal') {
-            if(!displayedNum.includes('.')) {
-                display.textContent = displayedNum + '.';
+            if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.'
+            } else if (previousKeyType === 'operator') {
+                display.textContent = '0.';
             }
+            calculator.dataset.previousKeyType = 'decimal'; 
+        }
+
+        if (action === 'clear') {
+            calculator.dataset.previousKeyType = 'clear'
         }
 
         if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
@@ -71,9 +50,9 @@ keys.addEventListener('click', e => {
 
             display.textContent = calculate(firstValue, operator, secondValue);
         }
-        
     }
 })
+
 
 const calculate = (n1, operator, n2) => {
     let result = '';
