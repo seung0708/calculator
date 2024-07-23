8cd6f89042d2ece1341fbfb5603445534b909719
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-07-23 12:59:19 -0700

    trying to style for different buttons in css grid

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-//when a number or operator is clicked, the input will display the value
+//select the input tag for displaying the calculations
 const input = document.querySelector('input')
+
+//select all the buttons
 const buttons = document.querySelectorAll('button')
 buttons.forEach(button => {
+    //initialize the value for each button
+    let operators = /[*/+\-]/g
     let value = button.value;
     
+    //have each button listen for a click event
     button.addEventListener('click', () => {
+        //when a button is clicked, display the value in the input
         input.value += value;
+        //if the value is C, delete everything
         if (value === 'C') {
             input.value = ''
         } 
+        //if the value is <, delete the last number or operator
         if (value === '<') {
             input.value = input.value.substring(0, input.value.length - 1)
         } 
+
+        //if value is =, return the result
         if (value === '=') {
             input.value = evaluate(input.value)
             console.log(input.value)
         }
-        
+
+        //I also need a way to replace an operator when it already exists. 
+        if (input.value.match(operators) && input.value[input.value.length - 1].match(operators)) {
+            console.log('this ran')
+            if(value.match(operators)) {
+                console.log('this ran too')
+                input.value = input.value.replace(value.includes(operators), value)
+                console.log(input.value)
+            }
+        }
     })
 })
 
